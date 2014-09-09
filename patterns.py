73ffd8b4c29dc6895a9d0db8282f82ce7cb2a0b8
ayu <mac.ayu15@gmail.com>
2014-09-09 11:57:32 +0900

    add samples

---
+++ patterns.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from sample import (
+    demo1,
+)
+
+
+patterns = [
+    (r'^!ping$', demo1.ping),
+    (r'^!dice$', demo1.throw_dice),
+    (r'^!parrot$', demo1.parrot),
+    ('hi', demo1.greet),
+    ('yay', lambda msg: 'yay!'),
+]
---
+++ sample/demo1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from random import randint 
+
+
+def ping(msg):
+    '''ping!'''
+    return 'pong'
+
+
+def greet(msg):
+    '''bot greets you'''
+    name = msg.FromHandle
+    return 'Hello, %s' % name
+
+
+def throw_dice(msg):
+    result = randint(1, 7)
+    return '%d' % result
+
+
+def parrot(msg):
+    text = msg.Body
+    return text
